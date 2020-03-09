a95909d34698527fa4008a854842a1cfd544e8d7
yarbob11 <dadser803@gmail.com>
2020-03-09 15:30:58 +0900

    Create React Project

---
+++ npm-react-test/management/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render(){
+    return (
+    <div className="gray-background">
+      <img src={logo} lat ="logo"/>
+      <h2>BIL</h2>
+    </div>
+    );
+  }
+}
+export default App;
