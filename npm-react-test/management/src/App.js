31a4a2a236ccfff1a0fd9a30356a586340222005
yarbob11 <dadser803@gmail.com>
2020-03-10 13:02:03 +0900

    Creat Customer.js

--- npm-react-test/management/src/App.js
+++ npm-react-test/management/src/App.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Customer from './components/Customer';
+const customers = [
+  {
+  'id':1,
+  'image':'https://placehoimage.com/64/64/1',
+  'name':'홍길동',
+  'birthday':'941010',
+  'gender':'남자',
+  'job':'대학생'
+ },
+ {
+  'id':2,
+  'image':'https://placehoimage.com/64/64/2',
+  'name':'동길홍',
+  'birthday':'921010',
+  'gender':'남자',
+  'job':'대학생'
+ },
+ {
+  'id':3,
+  'image':'https://placehoimage.com/64/64/3',
+  'name':'길동홍',
+  'birthday':'931010',
+  'gender':'남자',
+  'job':'대학생'
+ }
+]
 class App extends Component {
   render(){
     return (
-    <div className="gray-background">
-      <img src={logo} lat ="logo"/>
-      <h2>BIL</h2>
-    </div>
+      <div>
+        {
+          customers.map(c=>{
+            return (<Customer
+              key = {c.id} 
+              id = {c.id}
+              image = {c.image}
+              name = {c.name}
+              birthday = {c.birthday}
+              gender = {c.gender}
+              job = {c.job}
+
+            />);
+          })
+        }
+      
+      </div>
     );
   }
 }
